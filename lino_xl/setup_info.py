d42b3fb52f0630716be88087f607ce7e27171a59
Luc Saffre <luc.saffre@gmail.com>
2016-02-22 06:08:27 +0200

    http://luc.lino-framework.org/blog/2016/0222.html

--- lino_xl/setup_info.py
+++ lino_xl/setup_info.py
@@ -18,12 +18,10 @@ SETUP_INFO = dict(
     version='1.0.0',
     install_requires=['lino', 'appy', 'fuzzy', ],
     tests_require=[],
-    description="A framework for writing desktop-like web applications "
-                "using Django and ExtJS : Lino Extensions Library",
+    description="Lino Extensions Library",
     license='BSD License',
     include_package_data=True,
     zip_safe=False,
-    obsoletes=['djangosite', 'north'],
     author='Luc Saffre',
     author_email='luc.saffre@gmail.com',
     url="http://www.lino-framework.org",
@@ -48,20 +46,7 @@ SETUP_INFO = dict(
   Topic :: Software Development :: Libraries :: Application Frameworks""".splitlines())
 
 SETUP_INFO.update(long_description="""\
-Lino is a high-level framework for writing desktop-like customized
-database applications based on `Django <https://www.djangoproject.com/>`_
-and `Sencha ExtJS <http://www.sencha.com/products/extjs/>`_.
-Lino applications are Django projects
-where the application developer does not need to write any
-URLconf, HTML nor CSS (`more <http://lino-framework.org/about/what.html>`__).
-
-Examples of Lino applications are
-`Lino Welfare <http://welfare.lino-framework.org>`__,
-`Lino Così <http://cosi.lino-framework.org>`__
-or
-`Lino Voga <http://voga.lino-framework.org>`__
-(`more <http://lino-framework.org/about/projects.html>`__)
-
+This is a collection of plugins for the Lino framework.
 """)
 
 SETUP_INFO.update(packages=[str(n) for n in """
