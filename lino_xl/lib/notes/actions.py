052990575698f4cc7380458f817c83c300209a65
Luc Saffre <luc.saffre@gmail.com>
2016-07-28 18:46:06 +0300

    http://luc.lino-framework.org/blog/2016/0728.html

--- lino_xl/lib/notes/actions.py
+++ lino_xl/lib/notes/actions.py
@@ -17,48 +17,23 @@
 # License along with Lino XL.  If not, see
 # <http://www.gnu.org/licenses/>.
 """
-Database models for `lino_xl.lib.notes`.
-
-.. autosummary::
+Defines actions used by this plugin.
 
 """
-
-import logging
-logger = logging.getLogger(__name__)
-
-from django.conf import settings
-
-from lino.api import rt
 
 from lino.modlib.notify.actions import NotifyingAction
 
 
 class NotableAction(NotifyingAction):
+    """Abstract base class for notifying actions that may create a system
+    note and a notification.
 
-    def get_system_note_type(self, ar):
-        """Expected to return either `None` (the default) or an existing
-        :class:`NoteType <lino_xl.lib.notes.models.NoteType>`
-        instance. If this is not `None`, then a notification will
-        still be emitted, but the system not will not be stored in the
-        database as a :class:`lino_xl.lib.notes.models.Note`.
-
-        """
-        return settings.SITE.site_config.system_note_type
-
+    """
     def emit_notification(self, ar, owner, **kw):
-        nt = self.get_system_note_type(ar)
-        if nt:
-            # obj = ar.selected_rows[0]
-            # owner = self.get_notify_owner(obj)
-            prj = owner.get_related_project()
-            if prj:
-                kw.update(project=prj)
-            note = rt.models.notes.Note(
-                event_type=nt, owner=owner,
-                subject=ar.action_param_values.notify_subject,
-                body=ar.action_param_values.notify_body,
-                user=ar.user, **kw)
-            note.save()
-
+        kw.update(
+            subject=ar.action_param_values.notify_subject,
+            body=ar.action_param_values.notify_body)
+        owner.emit_system_note(ar, **kw)
         super(NotableAction, self).emit_notification(ar, owner, **kw)
+
 
