3754056d595e4cbbe025842da10d2a580aadbf88
Luc Saffre <luc.saffre@gmail.com>
2017-02-01 14:12:33 +0200

    http://luc.lino-framework.org/blog/2017/0201.html

--- lino_xl/lib/polls/roles.py
+++ lino_xl/lib/polls/roles.py
@@ -1,14 +1,10 @@
-# Copyright 2015 Luc Saffre
+# Copyright 2015-2017 Luc Saffre
 # License: BSD (see file COPYING for details)
 """User roles for `lino_xl.lib.polls`.
 
-This can be used directly as :attr:`user_types_module
-<lino.core.site.Site.user_types_module>` for simple applications,
-e.g.  :mod:`lino.projects.polly`.
 """
 
-from django.utils.translation import ugettext_lazy as _
-from lino.core.roles import UserRole, SiteUser, SiteAdmin
+from lino.core.roles import SiteUser, SiteAdmin
 
 
 class PollsUser(SiteUser):
@@ -27,11 +23,3 @@ class PollsAdmin(PollsStaff, SiteAdmin):
     pass
 
 
-from lino.modlib.users.choicelists import UserTypes
-
-
-UserTypes.clear()
-add = UserTypes.add_item
-add('000', _("Anonymous"), UserRole, name='anonymous', readonly=True)
-add('100', _("User"), PollsUser, name='user')
-add('900', _("Administrator"), PollsAdmin, name='admin')
