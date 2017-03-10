01a7cc6b10ce3215f1e962c7ae8bb741f9e3e666
Luc Saffre <luc.saffre@gmail.com>
2017-03-10 21:04:31 +0200

    http://luc.lino-framework.org/blog/2017/0310.html

--- lino_xl/setup_info.py
+++ lino_xl/setup_info.py
@@ -120,6 +120,7 @@ lino_xl.lib.humanlinks
 lino_xl.lib.humanlinks.fixtures
 lino_xl.lib.lists
 lino_xl.lib.lists.fixtures
+lino_xl.lib.mailbox
 lino_xl.lib.notes
 lino_xl.lib.notes.fixtures
 lino_xl.lib.outbox
