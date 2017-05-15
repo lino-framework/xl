0bc8fe6a91a374b8393f950ab5622071d264e1d5
Tonis piip <Tonis.piip@gmail.com>
2017-05-15 14:12:10 +0300

    bug-fix with meeting memebers

--- lino_xl/lib/tickets/mixins.py
+++ lino_xl/lib/tickets/mixins.py
@@ -4,6 +4,7 @@
 
 from lino.api import dd, rt, _
 from lino_xl.lib.votes.choicelists import VoteStates
+from lino_xl.lib.stars.mixins import get_favourite
 
 class Milestone(dd.Model):
     """Base class for models that can be used as
@@ -24,5 +25,12 @@ class Milestone(dd.Model):
         participants = list(self.get_milestone_users())
         for wish in self.wishes_by_milestone.all():
             for user in participants:
-                wish.ticket.set_auto_vote(user, VoteStates.invited)
-                
+                if dd.is_installed('votes'):
+                    wish.ticket.set_auto_vote(user, VoteStates.invited)
+                if dd.is_installed('stars'):
+                    star = get_favourite(wish.ticket, user=user)
+                    if star is None:
+                        Star = rt.modules.stars.Star
+                        star = Star(owner=wish.ticket, user=user)
+                        star.save()
+
