483567afac227212585799adbdabef68838e02be
Tonis piip <Tonis.piip@gmail.com>
2017-06-01 14:08:03 +0300

    Update slave summary for github.CommitsByTicket

--- lino_xl/lib/github/desktop.py
+++ lino_xl/lib/github/desktop.py
@@ -80,9 +80,9 @@ class CommitsByTicket(Commits):
         html += "<ul>"
         for c in sar:
             #todo have another js button that will expend the summary into the complete description.
-            html += "<li><a href={commit_url}>{sha}</a>:{user}:{date}<br/>{summary}</li>".format(
+            html += u"<li><a href={commit_url}>{sha}</a>:{user}:{date}<br/>{summary}</li>".format(
                 commit_url=c.url,
-                sha=c.sha[:6],
+                sha=c.sha[:5] + ":" + str(c.repository),
                 user=E.tostring(ar.obj2html(c.user,str(c.user))),
                 date=E.tostring(ar.obj2html(c, naturaltime(c.created),title = c.created.strftime('%Y-%m-%d %H:%M'))),
                 summary=c.summary,
