7e0f1ad3bc5b42367e8ecba56b745c2303a47cae
Luc Saffre <luc.saffre@gmail.com>
2017-10-05 12:00:36 +0300

    http://luc.lino-framework.org/blog/2017/1005.html

--- lino_xl/lib/tickets/ui.py
+++ lino_xl/lib/tickets/ui.py
@@ -552,6 +552,12 @@ class Tickets(dd.Table):
         return s
 
     @classmethod
+    def get_queryset(self, ar, **filter):
+        return self.model.objects.select_related(
+            'user', 'assigned_to', 'project',
+            'duplicate_of', 'end_user')
+        
+    @classmethod
     def get_request_queryset(self, ar):
         qs = super(Tickets, self).get_request_queryset(ar)
         pv = ar.param_values
