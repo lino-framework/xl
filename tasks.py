9d6ae33858a39c3b846b10b996a091ca794b7458
Luc Saffre <luc.saffre@gmail.com>
2018-04-29 18:50:24 +0300

    http://luc.lino-framework.org/blog/2018/0429.html

--- tasks.py
+++ tasks.py
@@ -1,11 +1,10 @@
-from atelier.invlib.ns import ns
-ns.setup_from_tasks(
+from atelier.invlib import setup_from_tasks
+ns = setup_from_tasks(
     globals(), "lino_xl",
     languages="en de fr et nl pt-br es".split(),
     tolerate_sphinx_warnings=False,
     blogref_url='http://luc.lino-framework.org',
     revision_control_system = 'git',
-    locale_dir='lino_xl/lib/xl/locale',
-    doc_trees= [])
+    locale_dir='lino_xl/lib/xl/locale')
                     
 
