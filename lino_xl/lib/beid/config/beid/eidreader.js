e1ebd1c46ae19c171703ba5d1cd099830804498a
Luc Saffre <luc.saffre@gmail.com>
2018-10-03 07:11:37 +0300

    testing with beid popup window

--- lino_xl/lib/beid/config/beid/eidreader.js
+++ lino_xl/lib/beid/config/beid/eidreader.js
@@ -18,8 +18,9 @@ Lino.beid_read_card_processor = function() {
     // var card = {}
     var my_id = uuid();
     var url = "{{settings.SITE.plugins.beid.urlhandler_prefix}}" + window.location.origin + "/eid/" + my_id;
+    console.log("Gonna open", url);
     var popup = window.open(url);
-    popup.close();
+    // popup.close();
     return { uuid: my_id };
     
     // var xhttp = new XMLHttpRequest();
