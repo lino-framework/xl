f3c602401975e90a5734fb16e5365cca7c809b5d
Luc Saffre <luc.saffre@gmail.com>
2019-03-11 16:42:49 +0200

    gonna release

--- lino_xl/setup_info.py
+++ lino_xl/setup_info.py
@@ -29,7 +29,7 @@ else:
 
 SETUP_INFO = dict(
     name='lino-xl',
-    version='19.2.0',
+    version='19.3.0',
     install_requires=install_requires,
     tests_require=[],
     description="Lino Extensions Library",
