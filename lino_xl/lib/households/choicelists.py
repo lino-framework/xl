1f5f39858e73b420917971e2fd88d98cde418c0c
Luc Saffre <luc.saffre@gmail.com>
2019-05-08 23:32:23 +0300

    http://luc.lino-framework.org/blog/2019/0508.html

--- lino_xl/lib/households/choicelists.py
+++ lino_xl/lib/households/choicelists.py
@@ -1,18 +1,12 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2012-2017 Rumma & Ko Ltd
-#
+# Copyright 2012-2019 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
-"""Database models for `lino_xl.lib.households`.
-
-"""
 
 from lino.api import dd, _
 from lino_xl.lib.contacts.roles import ContactsStaff
 
 
 class MemberDependencies(dd.ChoiceList):
-    """The list of allowed choices for the `charge` of a household member.
-    """
     verbose_name = _("Dependency")
     verbose_name_plural = _("Household Member Dependencies")
 
@@ -23,10 +17,6 @@ add('03', _("At shared charge"), 'shared')
 
 
 class MemberRoles(dd.ChoiceList):
-    """The list of allowed choices for the (:attr:`role
-    <lino_xl.lib.households.models.Member.role>` of a household member.
-
-    """
     required_roles = dd.login_required(ContactsStaff)
     verbose_name = _("Role")
     verbose_name_plural = _("Household member roles")
@@ -39,6 +29,7 @@ add('04', _("Cohabitant"), 'cohabitant')  # not a relative but living here
 add('05', _("Child"), 'child')  # i.e. of at least one parent
 add('06', _("Relative"), 'relative')  # relative who does not live here
 add('07', _("Adopted child"), 'adopted')
+add('08', _("Foster-child"), 'foster')
 add('10', _("Other"), 'other')  # neither cohabitant nor relaive
 # add('10', _("Child of head"), 'child_of_head')
 # add('11', _("Child of partner"), 'child_of_partner')
