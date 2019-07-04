08bd3897092737e4ade5808d91ee4c27ae2db419
Luc Saffre <luc.saffre@gmail.com>
2019-07-04 22:09:19 +0300

    http://luc.lino-framework.org/blog/2019/0704.html

--- lino_xl/setup_info.py
+++ lino_xl/setup_info.py
@@ -29,7 +29,7 @@ else:
 
 SETUP_INFO = dict(
     name='lino-xl',
-    version='19.7.1',
+    version='19.7.2',
     install_requires=install_requires,
     tests_require=[],
     description="Lino Extensions Library",
