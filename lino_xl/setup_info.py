2d75be2beb817058317b1c91cb8d99dcbb74bfd4
Luc Saffre <luc.saffre@gmail.com>
2019-09-13 10:53:55 +0300

    version 19.9.1

--- lino_xl/setup_info.py
+++ lino_xl/setup_info.py
@@ -29,7 +29,7 @@ from __future__ import unicode_literals
 
 SETUP_INFO = dict(
     name='lino-xl',
-    version='19.9.0',
+    version='19.9.1',
     install_requires=['lino', 'odfpy'],
     tests_require=[],
     description="Lino Extensions Library",
