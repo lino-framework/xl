0c84037e0d4b53783a3a8f33ec7c62ded662422c
Luc Saffre <luc.saffre@gmail.com>
2020-07-20 20:42:21 +0300

    release to pypi

--- lino_xl/setup_info.py
+++ lino_xl/setup_info.py
@@ -14,7 +14,7 @@
 
 SETUP_INFO = dict(
     name='lino-xl',
-    version='20.7.3',
+    version='20.7.4',
     install_requires=['lino'],  # odfpy dependency now in lino_xl.lib.appypod
     tests_require=[],
     description="Lino Extensions Library",
